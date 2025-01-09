06061ceb7a87a7f452d7bf0e7611952fe5399ade
SkinnyJosiah <josiahmedina0318@gmail.com>
2025-01-08 19:10:58 -0700

    Autonomous first functions. ignore blue autonomous for now.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutonomous.java
@@ -13,8 +13,17 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
+
+
 @Autonomous
 public class RedAutonomous extends LinearOpMode {
+
+    //Servo intakeTilt = null;
+    //Servo intake = null;
+    //Servo xfer = null;
+    //Servo leftIntake = null;
+    //Servo rightIntake = null;
+
     @Override
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
@@ -31,7 +40,15 @@ public class RedAutonomous extends LinearOpMode {
 
         if(isStopRequested()) return;
 
+        //intakeTilt.setPosition(0.75); // based on levi's code
+        //intake.setPosition(0);
+        //rightIntake.setPosition(0.06);
+        //leftIntake.setPosition(0.94);
+        //xfer.setPosition(0.15);
+
         drive.followTrajectory(traj1);
+
         drive.followTrajectory(traj2);
+
     }
 }
