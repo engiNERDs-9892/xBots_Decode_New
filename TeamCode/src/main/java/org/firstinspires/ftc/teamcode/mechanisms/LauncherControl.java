f93f25431d1cfe099bb2ba957d7adc662bbc4d03
Hudson <hudson_meng@s.thevillageschool.com>
2025-11-02 00:00:07 -0500

    Made launch control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LauncherControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LauncherControl.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class LauncherControl {
+
+    private DcMotor launchMotor;
+    private double ticksPerRotation;
+
+    public void init(HardwareMap hardwareMap){
+        launchMotor = hardwareMap.get(DcMotor.class, "shooter");
+        launchMotor.setDirection(DcMotor.Direction.REVERSE);
+        launchMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ticksPerRotation = launchMotor.getMotorType().getTicksPerRev();
+
+    }
+
+    public void launchBall(double power) {
+        launchMotor.setPower(power);
+    }
+
+    public double getLaunchSpeed() {
+        return launchMotor.getCurrentPosition() / ticksPerRotation;
+    }
 }
