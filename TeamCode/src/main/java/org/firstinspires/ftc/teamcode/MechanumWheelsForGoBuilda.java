1955145c9e7f224f8a4bc26410e980b8bfa6b0e0
TopProjectsCreator <ishaanbans@gmail.com>
2025-11-19 17:25:04 -0800

    Fix right stick X inversion for mechanum wheels
    Invert the right stick X value for correct wheel movement.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumWheelsForGoBuilda.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumWheelsForGoBuilda.java
@@ -32,7 +32,7 @@ public class MechanumWheelsForGoBuilda extends OpMode {
     public void loop() {
         double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
         double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-        double rx = gamepad1.right_stick_x;
+        double rx = -gamepad1.right_stick_x;
 
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio,            // but only if at least one is out of the range [-1, 1]
